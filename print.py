4ea69f81eb0900c9541dcea879759c3ef556a696
eric <276375508@qq.com>
2018-09-27 20:32:36 +0800

    update

---
+++ print.py
@@ -0,0 +1,23 @@
+# print.py
+
+print("   *   ")
+print("  ***  ")
+print(" ***** ")
+print("*******")
+
+a = 16
+b = 216 // a
+c = 216 % a
+print(b,c)
+
+hour = 63320 // 3600
+minute = 63320 % 3600 // 60
+secend = 63320 % 3600 % 60
+print(hour,minute,secend)
+
+hs = 100
+ss = 5.0 / 9.0 * (hs - 32)
+ks = ss + 273.15
+
+print(ss)
+print(ks)
